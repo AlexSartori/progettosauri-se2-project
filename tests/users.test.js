d86af61583243db4aa637eb330635c90346c3874
FraLotito <lotitoqf@gmail.com>
2018-11-26 22:42:13 +0100

    add tests create user

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,9 +1,29 @@
 const conc = require('../endpoints/users').create_user
 
-test('concat test', () => {
-    expect(conc('a','b')).toBe('ab');
+test('create_user invalid parameters', () => {
+    let fetch = require('node-fetch');
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: '{"name": "francesco"}'
+    }).then(response => {
+        expect(response.status).toEqual(400);
+        return response.status;
+    }).catch(response => {
+        return response.status;
+    });
 });
 
-test('concat null', () => {
-    expect(conc(null,null)).toBe(0);
+test('create_user valid parameters', () => {
+    let fetch = require('node-fetch');
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: '{"name": "testname", "surname": "testsurname", "email": "mail@test.test", "password": "testpwd"}'
+    }).then(response => {
+        expect(response.status).toEqual(201);
+        return response.status;
+    }).catch(response => {
+        return response.status;
+    });
 });
