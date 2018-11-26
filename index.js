84641e499cf1349ccd92005514c0a561a30e7a83
Niccolò Marcon <marcon.niccolo@gmail.com>
2018-11-26 18:29:01 +0100

    Add automatic endpoint registration

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const fs = require('fs');
+
+const endpoints_dir = 'endpoints'; // The directory of the endpoints definitions
 const PORT = process.env.PORT || 3000;
 let app = express();
 
-const users = require('./endpoints/users');
-users.register_endpoints(app);
+// Call register_endpoints for each file
+fs.readdirSync(endpoints_dir).forEach((file) => {
+  let register = require(`./${endpoints_dir}/${file}`).register_endpoints;
+  register(app);
+});
 
 server = app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
 
