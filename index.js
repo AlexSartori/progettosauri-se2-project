d0a6d4f13ae32bcc8db5617d02fbffd2894c94b4
Niccolò Marcon <marcon.niccolo@gmail.com>
2018-11-27 10:27:59 +0100

    Add body-parser

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
 const endpoints_dir = 'endpoints'; // The directory of the endpoints definitions
 const PORT = process.env.PORT || 3000;
 let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Call register_endpoints for each file
 fs.readdirSync(endpoints_dir).forEach((file) => {
