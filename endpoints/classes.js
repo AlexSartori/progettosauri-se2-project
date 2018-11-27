7f35a485c68b7ecf27fdc6c7b7a015034bf8ae21
Niccolò Marcon <marcon.niccolo@gmail.com>
2018-11-27 10:29:20 +0100

    Implement POST /classes

--- endpoints/classes.js
+++ endpoints/classes.js
@@ -1,9 +1,51 @@
-function register_endpoints(app) {
+const DB = require('../DinoBase');
 
+function register_endpoints(app) {
+  app.post('/classes', create_class);
 }
 
 function create_class(req, res) {
+  // Checks if the name is provided and is a string
+  let valid = req.body.name != undefined;
+  valid &= typeof(req.body.name) == 'string';
 
+  // Checks if the user id is provided and stores it
+  let user = req.get('user') || '';
+  valid &= user != '';
+
+  if (valid) {
+    new_class = {
+      'name': req.body.name,
+      'creator': parseInt(user),
+      'users': []
+    }
+
+    DB.edit_data(data => {
+      // Checks if the creator is an existing users
+      valid &= data.users[new_class.creator] != undefined;
+
+      // Checks which user ids are existing and adds them in the class
+      if (valid && Array.isArray(req.body.users)) {
+        req.body.users.forEach(class_member => {
+          if (data.users[class_member] != undefined) {
+            new_class.users.push(class_member);
+          }
+        });
+      }
+
+      // Writes in the DB
+      if (valid) {
+        new_class.id = data.classes_next_id++;
+        data.classes[new_class.id] = new_class;
+      }
+    });
+  }
+
+  if (valid)
+    res.status(201);
+  else
+    res.status(400);
+  res.send();
 }
 
 module.exports = {register_endpoints, create_class};
--- tests/endpoints/classes.test.js
+++ tests/endpoints/classes.test.js
@@ -1,11 +1,41 @@
 const classes = require('../../endpoints/classes');
+const DB = require('../../DinoBase');
 
 const fetch = require('node-fetch');
+const fs = require('fs');
 
-const app = require('../../index').app
-const server = require('../../index').server
-const PORT = process.env.PORT || require('../../index').PORT;
+const app = require('../../index').app;
+const server = require('../../index').server;
+const PORT = require('../../index').PORT;
 const BASE_URL = `http://localhost:${PORT}/classes`;
+
+beforeAll(() => {
+  process.env.TESTING = true;
+  fs.writeFileSync(DB.DB_TEST_PATH, JSON.stringify({
+    'classes': {},
+    'classes_next_id': 0,
+    'users': {
+      '0': {
+        'id': 0,
+        'name': 'test_user',
+        'mail': 'mail@test.com',
+        'password': 'test'
+      },
+      '1': {
+        'id': 1,
+        'name': 'other_test_user',
+        'mail': 'other_mail@test.com',
+        'password': 'test'
+      }
+    },
+    'users_next_id': 2
+  }));
+});
+
+afterAll(() => {
+  fs.writeFileSync(DB.DB_TEST_PATH, '{}');
+  server.close();
+});
 
 test('register_endpoints', () => {
   expect.assertions(1);
@@ -15,8 +45,84 @@ test('register_endpoints', () => {
   });
 });
 
-test('create_class', () => {
-
+test('successful_create_class', () => {
+  expect.assertions(6);
+  new_class = {
+    'name': 'Test class',
+    'users': [1]
+  };
+  return fetch(BASE_URL, {
+    method: 'POST',
+    body: JSON.stringify(new_class),
+    headers: {
+      'Content-Type': 'application/json',
+      'user': 0
+    }
+  })
+  .then(res => {
+    expect(res.status).toEqual(201);
+    DB.edit_data(data => {
+      obj = data.classes[0];
+      expect(obj.id).toEqual(0);
+      expect(obj.name).toEqual('Test class');
+      expect(obj.users.length).toEqual(1);
+      expect(data.users[obj.users[0]].name).toEqual('other_test_user');
+      expect(obj.creator).toEqual(0);
+    });
+  });
 });
 
-afterAll(() => server.close());
+test('failing_create_class_no_name', () => {
+  expect.assertions(1);
+  new_class = {
+    'users': [1]
+  };
+  return fetch(BASE_URL, {
+    method: 'POST',
+    body: JSON.stringify(new_class),
+    headers: {
+      'Content-Type': 'application/json',
+      'user': '0'
+    }
+  })
+  .then(res => {
+    expect(res.status).toEqual(400);
+  });
+});
+
+test('failing_create_class_no_creator', () => {
+  expect.assertions(1);
+  new_class = {
+    'name': 'test',
+    'users': [1]
+  };
+  return fetch(BASE_URL, {
+    method: 'POST',
+    body: JSON.stringify(new_class),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(res => {
+    expect(res.status).toEqual(400);
+  });
+});
+
+test('failing_create_class_no_name_string', () => {
+  expect.assertions(1);
+  new_class = {
+    'name': [],
+    'users': [1]
+  };
+  return fetch(BASE_URL, {
+    method: 'POST',
+    body: JSON.stringify(new_class),
+    headers: {
+      'Content-Type': 'application/json',
+      'user': 0
+    }
+  })
+  .then(res => {
+    expect(res.status).toEqual(400);
+  });
+});
