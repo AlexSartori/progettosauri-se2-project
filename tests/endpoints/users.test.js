59dc1f24d4a1f37dd230d18f1c3dad672df4a585
Mengoni Claudia (ACC0113094) <claudia.mengoni@sci-austud14x.unitn.it>
2018-11-29 11:44:42 +0100

    Add test_utils.js and some edits in user_test.js

--- tests/endpoints/users.test.js
+++ tests/endpoints/users.test.js
@@ -1,26 +1,28 @@
-const conc = require('../../endpoints/users').create_user;
-const fs = require('fs');
-const DB = require('../../DinoBase');
+//const conc = require('../../endpoints/users').create_user;
 const fetch = require('node-fetch');
 const server = require('../../index').server;
 const PORT = require('../../index').PORT;
 const BASE_URL = `http://localhost:${PORT}/`;
+const USER_URL = 'users/'
+const create_valid_user = require('../test_utils').create_valid_user
+const clean_db = require('../test_utils').clean_db
 
 beforeAll(() => {
   process.env.TESTING = true;
+  clean_db()
 });
 
 afterAll(() => {
-  fs.writeFileSync(DB.DB_TEST_PATH, '{}');
+  clean_db();
   server.close();
 });
 
 test('create_user invalid parameters', () => {
     expect.assertions(1);
-    return fetch('http://localhost:3000/users', {
+    return fetch(BASE_URL + USER_URL, {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
-        body: '{"name": "francesco"}'
+        body:'{"name": "francesco"'
     }).then(response => {
         expect(response.status).toEqual(400);
         return response.status;
@@ -31,7 +33,7 @@ test('create_user invalid parameters', () => {
 
 test('create_user valid parameters', () => {
     expect.assertions(1);
-    return fetch('http://localhost:3000/users', {
+    return fetch(BASE_URL + USER_URL, {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: '{"name": "testname", "surname": "testsurname", "email": "mail@test.test", "password": "testpwd"}'
@@ -46,88 +48,65 @@ test('create_user valid parameters', () => {
 //get_user_details test:
 //VALID TEST
 test('Get user test, valid ID, users exist', () => {
-    var user= {"users":[{
-        "key":0,
-             "value":{"email":"prova@dinomail.com",
-             "password":"dinopwd",
-             "name":"dinoname",
-             "surname":"dinosurname"}}]
-    }
-
-    fs.writeFileSync(DB.DB_TEST_PATH, JSON.stringify(user))
+    create_valid_user(0)
     expect.assertions(2);
     ID = 0
-    return fetch(BASE_URL + 'users/'+ ID,
-        {method: 'GET',
-        headers:{'Content-Type': 'application/json'}
-    }).then(res => {
+    return fetch(BASE_URL + USER_URL+ ID
+    ).then(res => {
         expect(res.status).toEqual(200);
         return res.json()
     }).then(text => {
         expect(text).toEqual(data['users'][0])
-        fs.writeFileSync(DB.DB_TEST_PATH, {})
+        clean_db()
     })
 });
 
 // UNVALID TESTS:
-// - ID is not an integer
+// - user_id is not an integer
 test('Get user test, unvalid ID, not an integer', () => {
     expect.assertions(2);
     ID= 'string'
-    return fetch(BASE_URL + 'users/'+ ID,
-        {method: 'GET',
-         headers:{'Content-Type': 'application/json'}
-    }).then(res => {
+    return fetch(BASE_URL + USER_URL+ ID
+    ).then(res => {
         expect(res.status).toEqual(400);
         return res.text();
     }).then(text =>
         expect(text).toEqual('Bad parameter, user_id should be a positive integer'));
  });
 
-// - ID is not a positive integer
+// - user_id is not a positive integer
 test('Get user test, unvalid ID, negative integer ', () => {
     expect.assertions(2);
     ID= '-1'
-    return fetch(BASE_URL + 'users/'+ ID,
-        {method: 'GET',
-        headers:{'Content-Type': 'application/json'}
-    }).then(res => {
+    return fetch(BASE_URL + USER_URL+ ID,
+    ).then(res => {
         expect(res.status).toEqual(400);
         return res.text();
     }).then(text =>
         expect(text).toEqual('Bad parameter, user_id should be a positive integer'));
 });
-/*
-// - ID is not in db
-test('Get user test, unvalid ID, not found', () => {
-   DB.edit_data((data) => {
-        ID = (Object.keys(data['users']).sort().length+1).toString()
-    });
 
-    ID = 40
+// - user_id is not in db
+test('Get user test, unvalid ID, not found', () => {
+    clean_db()
+    ID = 0
     expect.assertions(2);
-    return fetch(BASE_URL + 'users/'+ ID
+    return fetch(BASE_URL + USER_URL+ ID
     ).then(res => {
         expect(res.status).toEqual(404);
         return res.text();
     }).then(text =>
         expect(text).toEqual('User does not exist'));
-});*/
+});
 
 // Delete_user tests:
-// VALID TEST: (esiste ed è uguale)
+// VALID TEST: 
+// - user_id exists and it's equal to user in headers
 test('Delete user, valid test', () => {
-    var user= {"users":[{
-        "key":1,
-             "value":{"email":"prova@dinomail.com",
-             "password":"dinopwd",
-             "name":"dinoname",
-             "surname":"dinosurname"}}]
-  }
-    fs.writeFileSync(DB.DB_TEST_PATH, JSON.stringify(user))
+    create_valid_user(0);
     expect.assertions(2);
-    ID = 1
-    return fetch(BASE_URL + 'users/'+ ID, {
+    ID = 0
+    return fetch(BASE_URL + USER_URL+ ID, {
         method: 'DELETE',
         headers:{'Content-Type': 'application/json',
                  'user' : ID}
@@ -135,36 +114,38 @@ test('Delete user, valid test', () => {
         expect(res.status).toEqual(200);
         return res.text()
     }).then(text => {
-        fs.writeFileSync(DB.DB_TEST_PATH, {})
-        expect(text).toEqual("Success, account has been deleted")})
-
+       expect(text).toEqual("Success, account has been deleted")
+       clean_db()})
 });
 
 //UNVALID TESTS:
-// - user_id != req
-
+// - user_id != header user (both users have to exist)
 test('Delete user test, user_id is different from user in header', () => {
+    clean_db();
+    create_valid_user(0);
+    create_valid_user(1);
     expect.assertions(2);
     ID = 0
-    return fetch(BASE_URL + 'users/'+ ID, {
+    return fetch(BASE_URL + USER_URL+ ID, {
         method: 'DELETE',
         headers:{'Content-Type': 'application/json',
                  'user' : 1}
     }).then(res => {
         expect(res.status).toEqual(403);
         return res.text();
-    }).then(text =>
-        expect(text).toEqual("Permission denied, you are trying to delete an other user's account"));
+    }).then(text => {
+        expect(text).toEqual("Permission denied, you are trying to delete an other user's account")
+        clean_db()})
  });
 
-// - ID is not an integer
+// - user_id is not an integer (user_id should be equal to user in headers)
 test('Delete user test, unvalid user_id, not an integer', () => {
     expect.assertions(2);
     ID= 'string'
-    return fetch(BASE_URL + 'users/'+ ID, {
+    return fetch(BASE_URL + USER_URL+ ID, {
         method: 'DELETE',
         headers:{ 'Content-Type': 'application/json',
-                  'user' : 0}
+                  'user' : ID}
     }).then(res => {
         expect(res.status).toEqual(400);
         return res.text();
@@ -173,14 +154,14 @@ test('Delete user test, unvalid user_id, not an integer', () => {
  });
 
 
-// - ID is not a positive integer
+// - user_id is not a positive integer (user_id should be equal to user in headers)
 test('Delete user test, unvalid user_id, negative integer ', () => {
     expect.assertions(2);
-    ID= '-1'
-    return fetch(BASE_URL + 'users/'+ ID, {
+    ID= -1
+    return fetch(BASE_URL + USER_URL+ ID, {
         method: 'DELETE',
         headers:{'Content-Type': 'application/json',
-                 'user' : 0}
+                 'user' : ID}
     }).then(res => {
         expect(res.status).toEqual(400);
         return res.text();
@@ -188,21 +169,19 @@ test('Delete user test, unvalid user_id, negative integer ', () => {
         expect(text).toEqual('Bad parameter, user_id should be a positive integer'));
 });
 
-/*
-// - user_id is not in db
+// - user_id is not in db (user_id should be equal to user in headers)
 test('Delete user test, user_ID not in db', () => {
+    clean_db();
     expect.assertions(2);
-    DB.edit_data((data) => {
-        ID = (Object.keys(data['users']).sort().length+1).toString()
-    });
-    return fetch(BASE_URL + 'users/'+ ID,  {
+    ID = 0
+    return fetch(BASE_URL + USER_URL+ ID,  {
         method: 'DELETE',
         headers: {'Content-Type': 'application/json',
-                  'user' : parseInt(ID)}
+                  'user' : ID}
     }).then(res => {
         expect(res.status).toEqual(404);
         return res.text();
     }).then(text =>
         expect(text).toEqual('User does not exist'));
 });
-*/
+
---
+++ tests/test_utils.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const DB = require('../DinoBase');
+
+function create_valid_user(x){
+    var user= {"users":[{
+        "key":x,
+             "value":{"email":"prova@dinomail.com",
+             "password":"dinopwd",
+             "name":"dinoname",
+             "surname":"dinosurname"}}]
+    }
+    fs.writeFileSync(DB.DB_TEST_PATH, JSON.stringify(user))
+    return user
+}
+
+function create_unvalid_user(x){
+    var user= {"users":[{
+        "key":x,
+             "value":{"email":"prova@dinomail.com"}}]
+    }
+
+    fs.writeFileSync(DB.DB_TEST_PATH, JSON.stringify(user))
+    return user
+}
+
+function clean_db(){
+    fs.writeFileSync(DB.DB_TEST_PATH, {})
+    return
+}
+
+module.exports = {create_valid_user, clean_db, create_unvalid_user}
