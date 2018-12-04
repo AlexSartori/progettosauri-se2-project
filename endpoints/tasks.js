0a66cb578f00ad2d3727073ad2a207470e2c4282
FraLotito <lotitoqf@gmail.com>
2018-12-04 22:08:29 +0100

    add update_task

--- endpoints/tasks.js
+++ endpoints/tasks.js
@@ -5,6 +5,7 @@ function register_endpoints(app) {
   app.get('/tasks', get_available_tasks);
   app.delete('/tasks/:task_id', delete_task);
   app.get('/tasks/:task_id', get_task);
+  app.put('/tasks/:task_id', modify_task);
 }
 
 function create_task(req, res) {
@@ -110,21 +111,20 @@ function delete_task(req, res) {
 
 function get_available_tasks(req, res) {
   let result = [];
-  if(!req.params.user) {
+  let user = parseInt(req.get('user'));
+  console.log(user);
+  if(user==NaN) {
     res.status(400).send("Bad parameters");
   } else {
-    let user = parseInt(req.get('user'));
-    if(user != NaN) {
-      DB.read_data((data) => {
-        if (data.tasks) {
-          Object.keys(data.tasks).forEach(function(k) {
-            let task = data.tasks[k.toString()];
-            if(task.creator == user) result.push(k);
-          });
-        }
-      });
-      res.status(200).send(result);
-    }
+    DB.read_data((data) => {
+      if (data.tasks) {
+        Object.keys(data.tasks).forEach(function(k) {
+          let task = data.tasks[k.toString()];
+          if(task.creator == user) result.push(k);
+        });
+      }
+    });
+    res.status(200).send(result);
   }
 }
 
@@ -158,4 +158,68 @@ function get_task(req, res) {
   res.send(content);
 }
 
-module.exports = {register_endpoints, create_task, delete_task, get_available_tasks, get_task};
+function modify_task(req, res) {
+
+  // Checks if the text is provided and is a string and if the
+  let valid = req.body.text != undefined;
+  valid &= typeof(req.body.text) == 'string';
+
+  // Checks if the user id is provided and stores it
+  let user = parseInt(req.get('user'));
+  valid &= user != NaN;
+
+  if (valid) {
+    new_task = {
+      'text': req.body.text,
+      'answers': {},
+      'answers_next_id': 0,
+      'creator': user
+    }
+
+    DB.edit_data(data => {
+      // Checks if the user id is an existing user
+      valid &= data.users[user] != undefined;
+      valid &= data.tasks[req.params.task_id] != undefined;
+
+      // Checks if the multiple answers provided are valid
+      if (valid && Array.isArray(req.body.answers) && req.body.answers.length > 0) {
+
+        let validMultipleAnswers = 0;
+        req.body.answers.forEach(answer => {
+          if (typeof(answer.text) == 'string' && typeof(answer.correct) == 'boolean' ) {
+            validMultipleAnswers++;
+          }
+        });
+
+        //Only if all the multiple choice answers are valid the task is valid
+        if(validMultipleAnswers == req.body.answers.length && req.body.answers.length >= 2){
+          req.body.answers.forEach(answer => {
+
+            let new_answer = {
+                'text': answer.text,
+                'correct' : answer.correct
+            }
+
+            new_answer.id = new_task.answers_next_id++;
+            new_task.answers[new_answer.id] = new_answer;
+          });
+        } else {
+          valid = false;
+        }
+      }
+
+      // Writes in the DB if the creator is the user
+      if (valid && data.tasks[req.params.task_id].creator == user) {
+        data.tasks[req.params.task_id] = new_task;
+      }
+    });
+  }
+
+  if(valid)
+    res.status(201).send();
+  else
+    res.status(400).send();
+}
+
+module.exports = {register_endpoints, create_task, delete_task, 
+  get_available_tasks, get_task, modify_task};
