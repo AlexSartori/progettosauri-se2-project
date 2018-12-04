70e5ef4f7daa16810ffc542411beba00802a1987
Alex Sartori <alex.sartori1997@gmail.com>
2018-12-04 16:14:09 +0100

    Add implementation for PUT /task_groups

--- endpoints/task_groups.js
+++ endpoints/task_groups.js
@@ -4,13 +4,14 @@ function register_endpoints(app) {
     app.get('/task_groups', get_all_tgs);
     app.get('/task_groups/:tg_id', get_tg);
     app.post('/task_groups', create_tg);
+    app.put('/task_groups/:tg_id', edit_tg);
     app.delete('/task_groups/:tg_id', delete_tg);
 }
 
 function get_all_tgs(req, res) {
     let result = [];
 
-    DB.edit_data((data) => {
+    DB.read_data((data) => {
         if (data.task_groups)
             Object.keys(data.task_groups).forEach((k) => result.push(data.task_groups[k]));
     });
@@ -22,11 +23,35 @@ function get_tg(req, res) {
     if (!req.params.tg_id || isNaN(parseInt(req.params.tg_id)))
         res.status(400).send("Invalid ID");
     else {
-        DB.edit_data((data) => {
+        DB.read_data((data) => {
             if (data.task_groups && data.task_groups[req.params.tg_id])
                 res.status(200).send(data.task_groups[req.params.tg_id]);
             else
                 res.status(404).send("No such Task Group");
+        });
+    }
+}
+
+function edit_tg(req, res) {
+    let valid = true, param = req.body;
+    valid &= param.name != undefined && typeof(param.name) == 'string';
+    valid &= param.tasks != undefined && Array.isArray(param.tasks) &&
+             param.tasks.every((a) => typeof(a) == 'number');
+
+    if (!valid)
+        res.status(400).send("Bad parameters");
+    else if (!req.params.tg_id || isNaN(parseInt(req.params.tg_id)))
+        res.status(400).send("Invalid ID");
+    else {
+        DB.edit_data((data) => {
+            if (!data.task_groups || !data.task_groups[req.params.tg_id])
+                res.status(404).send("No such Task Group");
+            else {
+                new_tg = param;
+                new_tg.id = parseInt(req.params.tg_id);
+                data.task_groups[req.params.tg_id] = new_tg;
+                res.status(200).send('' + next_id);
+            }
         });
     }
 }
